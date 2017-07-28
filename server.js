bd55d61a8adb8d0328d10b98db82541187549c6f
Alex Marmon <alex@14four.com>
2017-07-28 08:47:16 -0700

    remove unneccesary stuffs

--- server.js
+++ server.js
@@ -1,45 +1,15 @@
 const webpack = require('webpack');
-const path = require('path');
 const WebpackDevServer = require('webpack-dev-server');
 const config = require('@lofty/lofty-webpack/webpack.dev.js');
-const express = require('express');
-const compress = require('compression');
-const bodyParser = require('body-parser')
-const router = require('./src/api/router');
 
-const app = express();
 const port = 3000;
 process.env['PORT'] = port;
 
-// Use router for API calls
-// app.use('/api', router);
-
-// Development Server - Hot Reload w/ WebpackDevServer & api proxy
-if (process.env.npm_lifecycle_event === 'dev') {
-  new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    hot: true,
-    historyApiFallback: true,
-    quiet: true,
-    // Create proxy for API with DevServer
-    proxy: {
-      '/api/*': 'http://localhost:' + (port + 30),
-    },
-  }).listen(port, 'localhost', (err) => {
-    if (err) { console.log(err); }
-  });
-  app.use(bodyParser.json());
-  app.listen(port + 30);
-} else {
-  // Production Server
-  app.use(bodyParser.json());
-  app.use(compress());
-  app.use(express.static('prod'));
-  app.use('/static', express.static('static'));
-  app.get('*', function(req, res){
-    res.sendFile(__dirname + '/prod/index.html');
-  });
-  app.listen(port);
-}
-
-module.exports = app;
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  quiet: true,
+}).listen(port, 'localhost', (err) => {
+  if (err) { console.log(err); }
+});
